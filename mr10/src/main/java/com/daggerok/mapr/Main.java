f841dff1f8525f4301057c8154b3d6eadde92b8a
Maksim Kostromin <Maksim.Kostromin@sigma.software>
2015-08-12 18:27:11 +0300

    mr10: running multiple jobs example

--- mr10/src/main/java/com/daggerok/mapr/Main.java
+++ mr10/src/main/java/com/daggerok/mapr/Main.java
@@ -4,14 +4,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         /*
         $ gradle :mr10:fatJar
-        $ scp mr10/build/libs/mr10-1.0.jar user01@maprdemo:~/mr10.jar
-        $ scp mr10/src/test/resources/university.txt user01@maprdemo:~/i10
+        $ scp mr10/build/libs/mr10-1.0.jar user01@maprdemo:~/mr10.jar && scp mr10/src/test/resources/university.txt user01@maprdemo:~/i10
         $ ssh user01@maprdemo
-        $ rm -rf stats.txt out10*
-        $ hadoop jar mr10.jar -D var1="verbal" -D var2="math" /user/user01/i10 /user/user01/out101 /user/user01/out102
-        $ cat stats.txt
-        $ cat out101/part-r-00000
-        $ cat out102/part-r-00000
+        $ rm -rf stats.txt && out10* && hadoop jar j10.jar -D var1="verbal" -D var2="math" /user/user01/i10 /user/user01/out101 /user/user01/out102
+        $ cat stats.txt out101/part-r-00000 out102/part-r-00000
         */
         Driver.runJob(args);
     }
